4ab9e391ee2ad4a87ec1447e9acb60efb1c9b2eb
saraiva.ufc <saraiva.ufc@gmail.com>
2014-11-05 22:18:15 -0300

    Vamos

--- src/br/ufc/clinic/view/ViewLogin.java
+++ src/br/ufc/clinic/view/ViewLogin.java
@@ -1,10 +1,6 @@
 package br.ufc.clinic.view;
 
-import java.awt.Dialog;
 import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.GraphicsConfiguration;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -67,85 +63,6 @@ public class ViewLogin extends JDialog {
 	@SuppressWarnings("deprecation")
 	public String getSenha(){
 		return this.senha.getText();
-	}
-	
-
-	public ViewLogin(Frame owner) {
-		super(owner);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Dialog owner) {
-		super(owner);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Window owner) {
-		super(owner);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Frame owner, boolean modal) {
-		super(owner, modal);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Frame owner, String title) {
-		super(owner, title);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Dialog owner, boolean modal) {
-		super(owner, modal);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Dialog owner, String title) {
-		super(owner, title);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Window owner, ModalityType modalityType) {
-		super(owner, modalityType);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Window owner, String title) {
-		super(owner, title);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Frame owner, String title, boolean modal) {
-		super(owner, title, modal);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Dialog owner, String title, boolean modal) {
-		super(owner, title, modal);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Window owner, String title, ModalityType modalityType) {
-		super(owner, title, modalityType);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Frame owner, String title, boolean modal,
-			GraphicsConfiguration gc) {
-		super(owner, title, modal, gc);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Dialog owner, String title, boolean modal,
-			GraphicsConfiguration gc) {
-		super(owner, title, modal, gc);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ViewLogin(Window owner, String title, ModalityType modalityType,
-			GraphicsConfiguration gc) {
-		super(owner, title, modalityType, gc);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Boolean isAcepted() {
